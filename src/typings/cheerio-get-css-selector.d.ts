644fbb61c9ba7f76693f57fa697f6fc5503ca42d
Karel-Jan Van Haute <karel-jan@statik.be>
2025-10-13 16:15:12 +0200

    remove

--- src/typings/cheerio-get-css-selector.d.ts
+++
@@ -1,3 +0,0 @@
-export declare module cheerioGetCssSelectors {
-  function init(): void;
-}
--- src/typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "pa11y-ci";
-declare module "cheerio-get-css-selector";
-
-declare interface CheerioWithCSS extends Cheerio {
-  getUniqueSelector(): string;
-}
